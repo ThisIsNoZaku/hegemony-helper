0e5324cd7ba2fcc969b7baaa100373e8e0aacb18
ThisIsNoZaku <damienmarble@gmail.com>
2026-02-13 22:06:00 -0800

    Show player bar on top of dialog backdrop.

--- src/components/PlayerBar.tsx
+++ src/components/PlayerBar.tsx
@@ -3,7 +3,7 @@ import type {PlayerClass} from "../data/game.ts";
 
 export function PlayerBar({onChange}: { onChange: (playerClass: PlayerClass) => void }) {
     // return <AppBar sx={{top: "1rem", height: "4rem", zIndex: 1000, maxWidth: 1280, margin: "0 auto", textAlign: "center"}}>
-        return <AppBar>
+        return <AppBar sx={{zIndex: 10000}}>
         <Toolbar variant="regular" sx={{justifyContent: "space-around"}}>
             <Button variant="contained" sx={{backgroundColor: "crimson"}} onClick={() => onChange("wc")}>Working Class</Button>
             <Button variant="contained" sx={{backgroundColor: "goldenrod"}} onClick={() => onChange("mc")}>Middle Class</Button>
