7f95a0e4d92ff71f434291da9e58a0ddc80bd94d
ThisIsNoZaku <damienmarble@gmail.com>
2026-02-13 21:29:38 -0800

    Set app bar position to fixed, so that it definitely won't scroll.

--- src/components/PlayerBar.tsx
+++ src/components/PlayerBar.tsx
@@ -2,8 +2,9 @@ import {AppBar, Button, Toolbar} from "@mui/material";
 import type {PlayerClass} from "../data/game.ts";
 
 export function PlayerBar({onChange}: { onChange: (playerClass: PlayerClass) => void }) {
-    return <AppBar position="sticky" sx={{top: "1rem", bottom: 'auto', marginBottom: 2, height: "4rem"}}>
-        <Toolbar variant="regular" sx={{justifyContent: "space-between"}}>
+    // return <AppBar sx={{top: "1rem", height: "4rem", zIndex: 1000, maxWidth: 1280, margin: "0 auto", textAlign: "center"}}>
+        return <AppBar>
+        <Toolbar variant="regular" sx={{justifyContent: "space-around"}}>
             <Button variant="contained" sx={{backgroundColor: "crimson"}} onClick={() => onChange("wc")}>Working Class</Button>
             <Button variant="contained" sx={{backgroundColor: "goldenrod"}} onClick={() => onChange("mc")}>Middle Class</Button>
             <Button variant="contained" sx={{backgroundColor: "blue"}} onClick={() => onChange("cc")}>Capitalist Class</Button>
