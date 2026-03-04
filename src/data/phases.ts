c3b95480b42dd6277747ab9525b174e5522e3e06
ThisIsNoZaku <damienmarble@gmail.com>
2026-03-03 19:05:04 -0800

    Phases definition.

---
+++ src/data/phases.ts
@@ -0,0 +1,50 @@
+import type {GoodsName} from "./goods.ts";
+import type {EmployeePlayerClass, EmployerPlayerClass} from "./players.ts";
+
+export interface ProductionPhaseResult {
+    /**
+     * Wages earned from other classes.
+     */
+    earnedWages?: Record<EmployerPlayerClass, number>
+    /**
+     * Wages paid to other classes.
+     */
+    paidWages?: Record<EmployeePlayerClass, number>
+    /**
+     * Goods produced during the production phase.
+     */
+    output: Record<GoodsName, number>
+}
+
+export interface TaxPhaseResult {
+    /**
+     * Income tax paid by employee classes.
+     */
+    incomeTaxPaid?: number,
+    /**
+     * Corporate tax paid by the capitalist class.
+     */
+    corporateTaxPaid?: number,
+    /**
+     * Employment tax paid by the capitalist and middle classes.
+     */
+    employmentTaxPaid?: number,
+    /**
+     * Income taxes received from mc and wc.
+     */
+    incomeTaxReceived?: Record<EmployeePlayerClass, number>
+    /**
+     * Corporate taxes received from cc.
+     */
+    corporateTaxReceived?: number,
+    /**
+     * Employment taxes received from cc and mc.
+     */
+    employmentTaxReceived?: Record<EmployerPlayerClass, number>
+}
+
+export interface ScoringPhaseResult {
+    pointsEarned: number
+}
+
+export const Phases = ["actions", "production", "taxes", "politics", "scoring"];
