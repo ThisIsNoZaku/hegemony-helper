34556cfd931d47967f32737c7797065ac8229998
ThisIsNoZaku <damienmarble@gmail.com>
2026-03-09 23:26:37 -0700

    Add buttons to automatically perform prosperity increase effects.

--- src/components/workers/ProsperityTrack.tsx
+++ src/components/workers/ProsperityTrack.tsx
@@ -1,16 +1,67 @@
 import {Radio, Stack} from "@mui/material";
 import Track from "../Track.tsx";
 import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
+import StarIcon from '@mui/icons-material/Star';
+import {PlayerActionBar} from "../PlayerActionBar.tsx";
+import {useContext} from "react";
+import {DispatchContext} from "../../state/GameContext.ts";
+import {getClassColor} from "../../utilities/getClassColor.ts";
+import {type WorkingClassPlayer} from "../../data/working-class/workingClass.ts";
+import type {UpdatePlayerAction} from "../../state/Reducers.ts";
+import type {MiddleClassPlayer} from "../../data/middle-class/middleClass.ts";
+import {EducationIcon, HealthIcon, LuxuryIcon} from "../Icons.tsx";
+import PersonIcon from "@mui/icons-material/Person";
 
-export default function ProsperityTrack({value, setValue, track, iconColor}: {
+export default function ProsperityTrack({value, setValue, track, player}: {
     value: number,
     track: number[],
     setValue: (value: number) => void,
-    iconColor: "crimson" | "goldenrod"
+    player: WorkingClassPlayer | MiddleClassPlayer
 }) {
+    const dispatch = useContext(DispatchContext);
+    // TODO: Consume resources when bumping.
+    const buttons = {
+        "prosperity-up": {
+            content: <>
+                <EducationIcon/>/<LuxuryIcon/>→ +<EmojiEmotionsIcon sx={{color: getClassColor(player.playerClass)}}/>
+            </>,
+            onClick: () => {
+                dispatch!({
+                    type: "update_player",
+                    player: "wc",
+                    playerData: {
+                        prosperity: Math.min(10, value + 1),
+                        points: player.points + track[Math.min(10, value + 1)]
+                    }
+                } as UpdatePlayerAction)
+            },
+            tooltip: "Increase Prosperity by 1."
+        },
+        "prosperity-up-healthcare": {
+            content: <>
+                <HealthIcon/> → → +<EmojiEmotionsIcon sx={{color: getClassColor(player.playerClass)}}/> + 2<StarIcon
+                sx={{color: getClassColor(player.playerClass)}}/> + <PersonIcon
+                sx={{color: getClassColor(player.playerClass)}}/>
+            </>,
+            onClick: () => {
+                dispatch!({
+                    type: "update_player",
+                    player: "wc",
+                    playerData: {
+                        population: player.population + 1,
+                        prosperity: Math.min(10, value + 1),
+                        points: player.points + track[Math.min(10, value + 1)] + 2
+                    }
+                } as UpdatePlayerAction)
+            },
+            tooltip: "Increase Prosperity by 1 and gain 2 bonus points. Add an unskilled worker to Unemployed workers box."
+        }
+    }
     return <>
-        <div style={{width: "100%", display: "flex", justifyContent: "center"}}><EmojiEmotionsIcon
-            sx={{color: iconColor}}/>Prosperity </div>
+        <div style={{width: "100%", display: "flex", justifyContent: "center"}}>
+            <EmojiEmotionsIcon sx={{color: getClassColor(player.playerClass)}}/>
+            <strong>Prosperity</strong>
+        </div>
         <Track size={11} elementProducer={({index}) => {
             return <Stack sx={{justifyContent: "center", alignItems: "center"}}>
                 <Radio checked={index === value}
@@ -19,5 +70,6 @@ export default function ProsperityTrack({value, setValue, track, iconColor}: {
                 {track[index]}
             </Stack>
         }}/>
+        <PlayerActionBar buttons={buttons}/>
     </>
 }
