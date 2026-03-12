0376d0448022b96ea302a85cea945aaa38d8b3e4
ThisIsNoZaku <damienmarble@gmail.com>
2026-03-12 10:07:14 -0700

    Non operational company has an output of 0.

--- src/utilities/calculateCompanyOutput.ts
+++ src/utilities/calculateCompanyOutput.ts
@@ -1,6 +1,9 @@
 import type {CompanyInstance} from "../data/companies.ts";
 
 export default function calculateCompanyOutput(company: CompanyInstance) {
+    if (!company.fullyAutomated && !company.workers) {
+        return 0;
+    }
     return company.output.base
         // @ts-ignore
         + (company.automatedBonus ? company.output.automationBonus : 0)
