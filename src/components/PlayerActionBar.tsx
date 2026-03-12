4d31d754d0cfde0ae5fcc2b52c4674008ba2124a
ThisIsNoZaku <damienmarble@gmail.com>
2026-03-12 10:09:21 -0700

    Made action bar configurable.

--- src/components/PlayerActionBar.tsx
+++ src/components/PlayerActionBar.tsx
@@ -1,17 +1,43 @@
-import {Button, Grid} from "@mui/material";
+import {Button, Grid, Tooltip} from "@mui/material";
+import type {ReactElement} from "react";
 
-export type AllowedButtons = "play_special_card" | "loans";
+type ButtonConfig = {
+    onClick: React.MouseEventHandler<HTMLButtonElement>,
+    content: ReactElement,
+    tooltip?: string
+}
 
 export function PlayerActionBar({buttons}: {
-    buttons: Record<string, React.MouseEventHandler<HTMLButtonElement>>
+    buttons: Record<string, React.MouseEventHandler<HTMLButtonElement> | ButtonConfig>
 }) {
-    return <Grid container size={4} columns={{xs: 3, sm: 6}} spacing={1}>
-        {Object.keys(buttons || {}).map(buttonText => {
-            return <Grid size={1} key={buttonText}>
-                <Button variant="contained" onClick={buttons[buttonText]}>
-                    {buttonText}
-                </Button>
-            </Grid>
+    return <Grid container size={4}
+                 columns={{xs: Math.min(Object.keys(buttons).length, 3), sm: Math.min(Object.keys(buttons).length, 6)}}
+                 spacing={1}>
+        {Object.keys(buttons || {}).map(buttonId => {
+            if (typeof buttons[buttonId] === "string") {
+                return <Grid size={1} key={buttonId}>
+                    <Button variant="contained" onClick={buttons[buttonId]}>
+                        {buttonId}
+                    </Button>
+                </Grid>
+            } else {
+                const config = buttons[buttonId] as ButtonConfig;
+                if (config.tooltip) {
+                    return <Grid size={1} key={buttonId}>
+                        <Tooltip title={config.tooltip}>
+                            <Button variant="contained" onClick={config.onClick}>
+                                {config.content}
+                            </Button>
+                        </Tooltip>
+                    </Grid>
+                } else {
+                    return <Grid size={1} key={buttonId}>
+                        <Button variant="contained" onClick={config.onClick}>
+                            {config.content}
+                        </Button>
+                    </Grid>
+                }
+            }
         })}
     </Grid>
 }
