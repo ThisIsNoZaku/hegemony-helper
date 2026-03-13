3f45522c627a131fa9ddbaa6b7abf6c4f4a0c3c9
ThisIsNoZaku <damienmarble@gmail.com>
2026-03-13 13:19:59 -0700

    Calculate owed wages for state.

--- src/components/simple/SimpleStateCalculator.tsx
+++ src/components/simple/SimpleStateCalculator.tsx
@@ -15,6 +15,15 @@ export default function SimpleStateCalculator({state, laws, sx}: {
     sx?: SxProps<Theme>
 }) {
     const dispatch = useContext(DispatchContext) as React.Dispatch<any>;
+    const owedWages = state.companies.reduce((wages, company) => {
+        if(company.workers) {
+            wages[company.workers] += company.wages[Math.max(laws.labor, company.wageLevel as number)];
+        }
+        return wages;
+    }, {
+        wc: 0,
+        mc: 0
+    });
     return <Paper sx={sx}><Stack spacing={1}>
         <strong>State</strong>
         <TextField label="Treasury" type="number"
@@ -62,6 +71,8 @@ export default function SimpleStateCalculator({state, laws, sx}: {
                         }
                     }}/>
             </Stack>
+        </Paper>
+        <Paper>
             <FormLabel component="label">
                 <strong>Public Companies</strong>
             </FormLabel>
@@ -71,6 +82,13 @@ export default function SimpleStateCalculator({state, laws, sx}: {
                              dispatch={dispatch}
                              laws={laws}
                              fixed={true}/>
+        </Paper>
+        <Paper>
+            <FormLabel component="legend"><strong>Owed Wages</strong></FormLabel>
+            <Stack direction="row" spacing={1} sx={{marginTop: "10px"}}>
+                <TextField sx={{flexGrow: 1}} label="To Working Class" value={owedWages["wc"]}/>
+                <TextField sx={{flexGrow: 1}} label="To Middle Class" value={owedWages["mc"]}/>
+            </Stack>
         </Paper>
     </Stack>
     </Paper>
