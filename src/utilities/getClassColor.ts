b3fdaa1b6d5a7098fb849833ebfd92ae001a6263
ThisIsNoZaku <damienmarble@gmail.com>
2026-03-15 15:53:11 -0700

    Can get alpha adjusted colors.

--- src/utilities/getClassColor.ts
+++ src/utilities/getClassColor.ts
@@ -1,14 +1,14 @@
 import type {PlayerClass} from "../data/players.ts";
 
-export function getClassColor(playerClass: PlayerClass) {
+export function getClassColor(playerClass: PlayerClass, alpha?: number) {
     switch (playerClass) {
         case "wc":
-            return "crimson";
+            return  `rgba(220,20,60, ${alpha ?? 1})`;
         case "mc":
-            return "goldenrod";
+            return `rgba(218,165,32, ${alpha ?? 1})`;
         case "state":
-            return "gray";
+            return `rgba(128,128,128, ${alpha ?? 1})`;
         case "cc":
-            return "blue";
+            return `rgba(0,0,255, ${alpha ?? 1})`;
     }
 }
