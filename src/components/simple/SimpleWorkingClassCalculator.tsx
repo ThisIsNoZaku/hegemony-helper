44525413ff971303071f0570788ac22823e5c33a
ThisIsNoZaku <damienmarble@gmail.com>
2026-03-30 14:18:57 -0700

    Add calculator for estimated food cost.

--- src/components/simple/SimpleWorkingClassCalculator.tsx
+++ src/components/simple/SimpleWorkingClassCalculator.tsx
@@ -7,7 +7,7 @@ import {
     Paper,
     Select,
     Stack,
-    TextField
+    TextField, Tooltip
 } from "@mui/material";
 import {IncomeTaxCalculator} from "../taxes/IncomeTaxCalculator.tsx";
 import type {WorkingClassPlayer} from "../../data/working-class/workingClass.ts";
@@ -27,6 +27,7 @@ import NumberSpinnerField from "../NumberSpinnerField.tsx";
 import _ from "lodash";
 import PersonIcon from "@mui/icons-material/Person";
 import {ForeignTradeIcon} from "../LawIcon.tsx";
+import PaidIcon from "@mui/icons-material/Paid";
 
 export default function ({wc, mc, cc, state, laws, sx}: {
     wc: WorkingClassPlayer,
@@ -45,6 +46,7 @@ export default function ({wc, mc, cc, state, laws, sx}: {
     }, {
         cc: 0,
         state: 0,
+        mc: mc ? 0 : undefined
     } as Record<PlayerClass, number>);
     (mc || {companies: []}).companies.filter(c => c).reduce((wages, c) => {
         if (c?.hasBonusWorker) {
@@ -88,18 +90,82 @@ export default function ({wc, mc, cc, state, laws, sx}: {
                 <Stack spacing={1}>
                     <FormLabel component="legend"><strong>Estimated Food Cost</strong></FormLabel>
                     <Stack spacing={1} direction="row">
-                        <FormControl sx={{flexGrow: 2}}>
+                        <FormControl sx={{flexGrow: 2, minWidth: 275, maxWidth: 275}}>
                             <InputLabel>Estimated Price of Food</InputLabel>
                             <Select variant={"outlined"}
+                                    sx={{width: "100%"}}
                                     value={estimatedFoodPrice}
                                     label="Estimated Price of Food"
-                                    onChange={e => setEstimatedFoodPrice(Number(e.target.value))}>
-                                <MenuItem value={9} sx={{alignContent: "center"}}>9 to <PersonIcon sx={{color: getClassColor("cc")}}/>/<PersonIcon sx={{color: getClassColor("mc")}}/></MenuItem>
-                                <MenuItem value={10}>10 to <ForeignTradeIcon/></MenuItem>
-                                <MenuItem value={12}>12 to  <PersonIcon sx={{color: getClassColor("cc")}}/>/<PersonIcon sx={{color: getClassColor("mc")}}/></MenuItem>
-                                <MenuItem value={15}>15 to  <PersonIcon sx={{color: getClassColor("cc")}}/>/<PersonIcon sx={{color: getClassColor("mc")}}/></MenuItem>
-                                <MenuItem value={15}>10 to <ForeignTradeIcon/> + 5 to <PersonIcon sx={{color: getClassColor("state")}}/></MenuItem>
-                                <MenuItem value={15}>10 to <ForeignTradeIcon/> + 10 to <PersonIcon sx={{color: getClassColor("state")}}/></MenuItem>
+                                    onChange={e => setEstimatedFoodPrice(Number(e.target.value))}
+                                    MenuProps={{
+                                        PaperProps: {
+                                            sx: {minWidth: 275}
+                                        }
+                                    }}>
+                                <MenuItem value={9} sx={{alignContent: "center"}}>
+                                    <Tooltip title={`9 to CC` + (mc ? " or MC" : "")} placement="right">
+                                        <span style={{
+                                            display: "flex",
+                                            alignItems: "center",
+                                            justifyContent: "center",
+                                            height: "100%",
+                                            width: "100%"
+                                        }}>9 to <PersonIcon
+                                            sx={{color: getClassColor("cc")}}/>{mc && <>/<PersonIcon
+                                            sx={{color: getClassColor("mc")}}/></>}</span>
+                                    </Tooltip>
+                                </MenuItem>
+                                <MenuItem value={10}>
+                                    <Tooltip title="10 to Foreign Trade" placement="right">
+                                        <span style={{
+                                            display: "flex",
+                                            alignItems: "center",
+                                            justifyContent: "center",
+                                            height: "100%",
+                                            width: "100%"
+                                        }}>10 to <ForeignTradeIcon/></span>
+                                    </Tooltip>
+                                </MenuItem>
+                                <MenuItem value={12}>
+                                    <Tooltip title={`12 to CC` + (mc ? " or MC" : "")} placement="right">
+                                        <span style={{
+                                            display: "flex",
+                                            alignItems: "center",
+                                            justifyContent: "center",
+                                            height: "100%",
+                                            width: "100%"
+                                        }}>12 to <PersonIcon
+                                            sx={{color: getClassColor("cc")}}/>{mc && <>/<PersonIcon
+                                            sx={{color: getClassColor("mc")}}/></>}</span>
+                                    </Tooltip>
+                                </MenuItem>
+                                <MenuItem value={15}>
+                                    <Tooltip title={`15 to CC` + (mc ? " or MC" : "")} placement="right">
+                                        <span style={{
+                                            display: "flex",
+                                            alignItems: "center",
+                                            justifyContent: "center",
+                                            height: "100%",
+                                            width: "100%"
+                                        }}>15 to <PersonIcon
+                                            sx={{color: getClassColor("cc")}}/>{mc && <>/<PersonIcon
+                                            sx={{color: getClassColor("mc")}}/></>}
+                                            or 10 to <ForeignTradeIcon/> + 5 to <PersonIcon
+                                                sx={{color: getClassColor("state")}}/>
+                                        </span>
+                                    </Tooltip>
+                                </MenuItem>
+                                <MenuItem value={20}>
+                                    <Tooltip title="10 to Foreign Trade + 10 to State" placement="right"><span style={{
+                                        display: "flex",
+                                        alignItems: "center",
+                                        justifyContent: "center",
+                                        height: "100%",
+                                        width: "100%"
+                                    }}>10 to <ForeignTradeIcon/> + 10 to <PersonIcon
+                                        sx={{color: getClassColor("state")}}/></span>
+                                    </Tooltip>
+                                </MenuItem>
                             </Select>
                         </FormControl>
                         <Box sx={{alignContent: "center"}}>
@@ -114,8 +180,11 @@ export default function ({wc, mc, cc, state, laws, sx}: {
                         <Box sx={{alignContent: "center"}}>
                             =
                         </Box>
-                        <TextField sx={{flexGrow: 1}} type="number" label="Estimated Total"
-                                   value={Math.floor(wc.population / 3) * estimatedFoodPrice}/>
+                        <Stack direction="row" sx={{alignContent: "center", alignItems: "center", flexGrow: 1}}>
+                            <PaidIcon/>
+                            <TextField sx={{flexGrow: 1}} type="number" label="Estimated Total"
+                                       value={Math.floor(wc.population / 3) * estimatedFoodPrice}/>
+                        </Stack>
                     </Stack>
                 </Stack>
             </Paper>
